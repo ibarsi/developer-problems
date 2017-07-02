deaf46d9bf01f6229f0627c5f81fac0edcfa0dc0
Igor Barsi <ibarsi@unioncreative.com>
2017-07-02 14:10:29 -0400

    INITIAL COMMIT

---
+++ main/logger.js
@@ -0,0 +1,26 @@
+const winston = require('winston');
+
+const winston_logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({ filename: '/var/log/developer-problems.log' })
+    ]
+});
+
+module.exports = {
+    info(message) {
+        if (process.env.NODE_ENV === 'production') {
+            winston_logger.info(message);
+        }
+        else {
+            console.log(message);
+        }
+    },
+    error(message) {
+        if (process.env.NODE_ENV === 'production') {
+            winston_logger.error(message);
+        }
+        else {
+            console.error(message);
+        }
+    }
+};
